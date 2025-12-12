6f2943dbe89cd65045007f9ab895d041e5663321
Tengz <redcraftrus@gmail.com>
2025-12-12 10:21:43 +0200

    refactor: update build script

--- org-chart/build.sh
+++ org-chart/build.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
 mkdir -p dist
-GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build -ldflags "-s -w" -o ./dist/org-chart org-chart.go && upx ./dist/org-chart
-GOARCH=amd64 GOOS=windows CGO_ENABLED=0 go build -ldflags "-s -w" -o ./dist/org-chart.exe org-chart.go && upx ./dist/org-chart.exe
+GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o ./dist/ org-chart.go
+GOARCH=amd64 GOOS=windows CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o ./dist/ org-chart.go
+
+if ! command -v upx > /dev/null 2>&1
+then
+  upx --best --lzma ./dist/org-chart ./dist/org-chart || true
+fi
 
 echo "Compilation completed, please find executables in dist directory."
