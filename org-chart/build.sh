8d366cbf277633d5d9a75ee9334114118ba9ad84
tengzl33t <nikita.ratskov2000@gmail.com>
2025-12-16 09:19:31 +0200

    fix build with upx

--- org-chart/build.sh
+++ org-chart/build.sh
@@ -4,7 +4,7 @@ mkdir -p dist
 GOARCH=amd64 GOOS=linux CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o ./dist/ org-chart.go
 GOARCH=amd64 GOOS=windows CGO_ENABLED=0 go build -trimpath -ldflags "-s -w" -o ./dist/ org-chart.go
 
-if ! command -v upx > /dev/null 2>&1
+if command -v upx > /dev/null 2>&1
 then
   upx --best --lzma ./dist/org-chart ./dist/org-chart || true
 fi
